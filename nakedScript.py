1610305225929f02affb8393b58ccb57780bc33a
nexor17 <69bropro@gmail.com>
2025-10-20 23:56:30 +0200

    Update nakedscript.py

--- nakedScript.py
+++ nakedScript.py
@@ -1,8 +1,13 @@
 import math
+import os
 from re import X
 import numpy as np
 import matplotlib.pyplot as plt
-y = np.loadtxt('data/GSPC.csv', delimiter=',', unpack=True, skiprows=1, usecols=4)
+# Load open, high, low, and close prices from the CSV
+input_csv = 'data/GSPC.csv'
+open_prices, high_prices, low_prices, y = np.loadtxt(
+    input_csv, delimiter=',', unpack=True, skiprows=1, usecols=(1, 2, 3, 4)
+)
 x = np.arange(len(y))
 
 xMat = np.matrix(x).T
@@ -31,9 +36,27 @@ def funct(x1, x, tau):
 yMat = np.matrix(y)
 tua = 0.9
 yPred = lwr(xCon, yMat.T, tua)
-plt.scatter(x, y)
-plt.plot(x, yPred)
-plt.show()
+
+# Combine all data columns for the output CSV
+output_data = np.vstack([x, open_prices, high_prices, low_prices, y, yPred]).T
+
+# Define output directory and filename
+output_dir = '/optidata'
+base_filename = os.path.splitext(os.path.basename(input_csv))[0]
+output_filename = f'{base_filename}Optimized.csv'
+output_path = os.path.join(output_dir, output_filename)
+
+# Create directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Save the combined data to the new CSV file
+header = 'dayoftheyear,open,high,low,close,WLLDE'
+np.savetxt(output_path, output_data, delimiter=',', header=header, comments='')
+
+#plt.scatter(x, y)
+#plt.plot(x, yPred)
+#plt.show()
 
 
     
